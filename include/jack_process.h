15ab1deb6600bf0f23d69970caa90850d7b91f3e
MajorX234 <majorx234@googlemail.com>
2024-11-12 23:54:08 +0100

    Add crate and clear function for jack stuff

--- include/jack_process.h
+++ include/jack_process.h
@@ -3,8 +3,11 @@
 #include <jack/jack.h>
 
 typedef struct JackStuff{
+  jack_client_t* client;
   jack_port_t* out_port[2];
   jack_ringbuffer_t* ringbuffer[2];
 } JackStuff;
 
 int process(jack_nframes_t nframes, void* jack_stuff_raw);
+JackStuff* create_jack_stuff(char* client_name);
+void jack_stuff_clear(JackStuff* jack_stuff);
--- src/jack_process.c
+++ src/jack_process.c
@@ -1,8 +1,9 @@
 #include "jack_process.h"
 #include <jack/types.h>
-#include <jack/ringbuffer.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <jack/jack.h>
+#include <jack/ringbuffer.h>
 
 int process(jack_nframes_t nframes, void* jack_stuff_raw)
 {
@@ -23,4 +24,41 @@ int process(jack_nframes_t nframes, void* jack_stuff_raw)
       }
   }
   return 0;
+}
+
+JackStuff* create_jack_stuff(char* client_name){
+  JackStuff* jack_stuff = (JackStuff*)malloc(sizeof(JackStuff));
+
+  jack_stuff->out_port[0] = NULL;
+  jack_stuff->ringbuffer[0] = NULL;
+  jack_stuff->out_port[1] = NULL;
+  jack_stuff->ringbuffer[1] = NULL;
+  jack_stuff->client = NULL;
+
+  jack_stuff->client = jack_client_open (client_name,
+                                        JackNullOption,
+                                        0,
+                                        0 );
+  const size_t ringbuffer_size = 4096 * sizeof(float);
+  for (size_t ch = 0; ch < 2; ch++){
+    jack_stuff->out_port[ch] = jack_port_register (jack_stuff->client,
+                                                   "output",
+                                                   JACK_DEFAULT_AUDIO_TYPE,
+                                                   JackPortIsOutput,
+                                                   0 );
+    jack_stuff->ringbuffer[ch] = jack_ringbuffer_create(ringbuffer_size);
+  }
+
+  jack_set_process_callback(jack_stuff->client, process, (void*)jack_stuff);
+  //client.set_sample_rate(48000);
+  jack_activate(jack_stuff->client);
+  return jack_stuff;
+}
+
+void jack_stuff_clear(JackStuff* jack_stuff) {
+  jack_deactivate(jack_stuff->client);
+  jack_client_close(jack_stuff->client);
+  jack_ringbuffer_free(jack_stuff->ringbuffer[0]);
+  jack_ringbuffer_free(jack_stuff->ringbuffer[1]);
+  free(jack_stuff);
 }
