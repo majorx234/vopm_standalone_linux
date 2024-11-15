c206d862955d9a1b4ec8400ab045c1c06274a60e
MajorX234 <majorx234@googlemail.com>
2024-11-15 01:51:55 +0100

    Add jack midi processing

--- src/jack_process.c
+++ src/jack_process.c
@@ -1,8 +1,9 @@
 #include "jack_process.h"
-#include <jack/types.h>
+#include <stdbool.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <jack/jack.h>
+#include <jack/types.h>
 #include <jack/ringbuffer.h>
 #include <jack/midiport.h>
 
@@ -10,9 +11,23 @@ int process(jack_nframes_t nframes, void* jack_stuff_raw)
 {
   JackStuff* jack_stuff = (JackStuff*)jack_stuff_raw;
   // midi_in event handling:
-  void* jack_midi_buffer =  jack_port_get_buffer ( jack_stuff->midi_in_port, nframes);
-  int event_count = jack_midi_get_event_count(jack_midi_buffer);
- 
+  void* jack_midi_in_buffer =  jack_port_get_buffer ( jack_stuff->midi_in_port, nframes);
+  int event_count = jack_midi_get_event_count(jack_midi_in_buffer);
+  jack_nframes_t last_frame_time = jack_last_frame_time(jack_stuff->client);
+
+  for (int i = 0;i<event_count;i++) {
+    jack_midi_event_t ev;
+    bool ok  = false;
+    int read = jack_midi_event_get(&ev, jack_midi_in_buffer, i);
+    if(ev.size == 3) {
+      size_t space = jack_ringbuffer_write_space(jack_stuff->midi_in_ringbuffer);
+      if (space>3) {
+        int written = jack_ringbuffer_write(jack_stuff->midi_in_ringbuffer, (char*)&ev, 3);
+      }
+    }
+  }
+
+  // audio out
   for (size_t ch = 0; ch < 2; ch++){
       float* outputBuffer = (float*)jack_port_get_buffer ( jack_stuff->out_port[ch], nframes);
 
@@ -34,6 +49,7 @@ JackStuff* create_jack_stuff(char* client_name){
   JackStuff* jack_stuff = (JackStuff*)malloc(sizeof(JackStuff));
 
   jack_stuff->midi_in_port = NULL;
+  jack_stuff->midi_in_ringbuffer = NULL;
   jack_stuff->out_port[0] = NULL;
   jack_stuff->ringbuffer[0] = NULL;
   jack_stuff->out_port[1] = NULL;
@@ -45,10 +61,11 @@ JackStuff* create_jack_stuff(char* client_name){
                                          0,
                                          0 );
   const size_t ringbuffer_size = 4096 * sizeof(float);
-    jack_stuff->midi_in_port  = jack_port_register (jack_stuff->client,
-                                                    "midi_input",
-                                                    JACK_DEFAULT_MIDI_TYPE,
-                                                    JackPortIsInput, 0);
+  jack_stuff->midi_in_port  = jack_port_register (jack_stuff->client,
+                                                  "midi_input",
+                                                  JACK_DEFAULT_MIDI_TYPE,
+                                                  JackPortIsInput, 0);
+  jack_stuff->midi_in_ringbuffer = jack_ringbuffer_create(1024);
   for (size_t ch = 0; ch < 2; ch++){
     jack_stuff->out_port[ch] = jack_port_register (jack_stuff->client,
                                                    "output",
@@ -67,6 +84,7 @@ JackStuff* create_jack_stuff(char* client_name){
 void jack_stuff_clear(JackStuff* jack_stuff) {
   jack_deactivate(jack_stuff->client);
   jack_client_close(jack_stuff->client);
+  jack_ringbuffer_free(jack_stuff->midi_in_ringbuffer);
   jack_ringbuffer_free(jack_stuff->ringbuffer[0]);
   jack_ringbuffer_free(jack_stuff->ringbuffer[1]);
   free(jack_stuff);
